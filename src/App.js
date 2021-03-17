4bfd131b30303f6f3e48d9bbeed3fb1e1b3e1c28
Marques <MBatoon1996@gmail.com>
2021-03-17 01:27:48 -1000

    added brand font awesome icons

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendar, faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCalendar, faCoffee);
+library.add(faCalendar, faCoffee, fab);
 
 
 const App = () => {
@@ -14,13 +15,13 @@ const App = () => {
       <h1 className="text-center m-3">Marques' Currency Converter</h1>
       <Home/>
       <footer>
-      <FontAwesomeIcon icon={['fab', 'apple']} />
-      <FontAwesomeIcon icon={['fab', 'microsoft']} />
-      <FontAwesomeIcon icon={['fab', 'google']} />
-      <FontAwesomeIcon icon={['fas', 'coffee']} />
-      <FontAwesomeIcon icon="calendar"/>
-      <FontAwesomeIcon icon="coffee" />
-      <p>End</p>
+        <FontAwesomeIcon icon={['fab', 'apple']} />
+        <FontAwesomeIcon icon={['fab', 'microsoft']} />
+        <FontAwesomeIcon icon={['fab', 'google']} />
+        <FontAwesomeIcon icon={['fas', 'coffee']} />
+        <FontAwesomeIcon icon="calendar"/>
+        <FontAwesomeIcon icon="coffee" />
+        <p>End</p>
       </footer>
     </div>
   )
